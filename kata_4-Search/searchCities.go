11b8d9baee31fde7d4d44a02b58ae0635af965f4
Sylamsh <srisylamsh.amrutakavi@amagi.com>
2023-09-07 15:18:18 +0530

    dirs name change

---
+++ kata_4-Search/searchCities.go
@@ -0,0 +1,3 @@
+package kata4search
+
+var cities = []string{"Paris", "Budapest", "Skopje", "Rotterdam", "Valencia", "Vancouver", "Amsterdam", "Vienna", "Sydney", "New York City", "London", "Bangkok", "Hong Kong", "Dubai", "Rome", "Istanbul"}
